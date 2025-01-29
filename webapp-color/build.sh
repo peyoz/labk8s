f0e3e2eb504804b3f1db0ab738f67e21ef6cd626
PEYRACHIA Alessandro <alessandro.peyrachia@nposervices.com>
2025-01-29 17:18:45 +0100

    update labs

--- webapp-color/build.sh
+++ webapp-color/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-tstamp="$(date '+%Y%m%d_%H%M%S')"
-docker build -t "akslab88lu9i.azurecr.io/webapp-color:${tstamp}" .
-docker push akslab88lu9i.azurecr.io/webapp-color:${tstamp}
+if [ -z "${1}" ]; then
+  tag="$(date '+%Y%m%d_%H%M%S')"
+else
+  tag="${1}"
+fi
+docker build -t "akslab88lu9i.azurecr.io/webapp-color:${tag}" .
+docker push "akslab88lu9i.azurecr.io/webapp-color:${tag}"
