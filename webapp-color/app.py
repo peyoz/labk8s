816f8bd5c7ec5abc74587b57f2d1e20505777eb6
PEYRACHIA Alessandro <alessandro.peyrachia@nposervices.com>
2025-01-30 11:25:03 +0100

    update 2.0

--- webapp-color/app.py
+++ webapp-color/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
-import socket
-import random
 import os
+import random
 import argparse
 from datetime import datetime
+import logging
+import socket
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+# Define color codes and supported colors
 color_codes = {
     "red": "#e74c3c",
     "green": "#16a085",
@@ -18,58 +21,68 @@ color_codes = {
 
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
-DATA_FILE_PATH = "/data/data-id.txt"
-STATIC = os.environ.get("STATIC", "false").lower() == "true"
+def get_color_from_args():
+    """Parse command line arguments to get the color."""
+    parser = argparse.ArgumentParser(description="A sample web application that displays a colored background.")
+    parser.add_argument('--color', choices=color_codes.keys(), help=f"Color to display. Must be one of {SUPPORTED_COLORS}.")
+    args = parser.parse_args()
+    return args.color
 
-name = socket.gethostname()
-contents = ""
-
-if STATIC:
-    os.makedirs(os.path.dirname(DATA_FILE_PATH), exist_ok=True)
-    timestamp_line = f"Startup executed: {datetime.now().isoformat()}\n"
-    with open(DATA_FILE_PATH, "a") as file:
-        file.write(timestamp_line)
-    with open(DATA_FILE_PATH, "r") as file:
-        contents = file.read()
-else:
-    print(f"Dynamic mode: 'name' variable initialized with the hostname.")
+def determine_color():
+    """Determine the color based on command line argument, environment variable, or random choice."""
+    color_from_arg = get_color_from_args()
+    if color_from_arg:
+        logging.info(f"Color from command line argument: {color_from_arg}")
+        return color_from_arg
+    elif COLOR_FROM_ENV := os.environ.get('APP_COLOR'):
+        logging.info(f"Color from environment variable: {COLOR_FROM_ENV}")
+        return COLOR_FROM_ENV
+    else:
+        color = random.choice(list(color_codes.keys()))
+        logging.info(f"No command line argument or environment variable. Picking a Random Color: {color}")
+        return color
 
 @app.route("/")
-def main():
+def main_route():
+    """Render the main page with the determined color and contents."""
+    global name, contents
+    COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+    COLOR = determine_color()
+
+    if COLOR not in color_codes:
+        logging.error(f"Color not supported. Received '{COLOR}', expected one of {SUPPORTED_COLORS}")
+        exit(1)
+
+    STATIC = os.environ.get("STATIC", "false").lower() == "true"
+
+    if STATIC:
+        data_file_path = "/data/data-id.txt"
+        os.makedirs(os.path.dirname(data_file_path), exist_ok=True)
+        timestamp_line = f"Startup executed: {datetime.now().isoformat()}\n"
+        with open(data_file_path, "a") as file:
+            file.write(timestamp_line)
+        with open(data_file_path, "r") as file:
+            contents = file.read()
+    else:
+        logging.info("Dynamic mode: 'name' variable initialized with the hostname.")
+
     return render_template('hello.html', name=name, color=color_codes[COLOR], contents=contents)
 
 if __name__ == "__main__":
+    # Initialize global variables
+    name = socket.gethostname()
+    contents = ""
 
-    print(" This is a sample web application that displays a colored background. \n"
-          " A color can be specified in two ways. \n"
-          "\n"
-          " 1. As a command line argument with --color as the argument. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 2. As an Environment variable APP_COLOR. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 3. If none of the above then a random color is picked from the above list. \n"
-          " Note: Command line argument takes precedence over the environment variable.\n"
-          "\n"
-          "")
+    # Print description of how the application works
+    print("This is a sample web application that displays a colored background.")
+    print("A color can be specified in two ways:")
+    print("\n1. As a command line argument with --color as the argument. Accepts one of " + SUPPORTED_COLORS)
+    print("2. As an Environment variable APP_COLOR. Accepts one of " + SUPPORTED_COLORS)
+    print("3. If none of the above then a random color is picked from the above list.")
+    print("Note: Command line argument takes precedence over the environment variable.\n")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through the environment variable -" + COLOR_FROM_ENV + ". However, the color from the command line argument takes precedence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
+    # Start the Flask application
     app.run(host="0.0.0.0", port=8080)
